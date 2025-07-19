b7d9c13d3540fe6a5afadee58a4eea36b0bc60c4
reeta-042 <155265356+reeta-042@users.noreply.github.com>
2025-07-19 05:57:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import os
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Barcode Lookup API",
     description="An intelligent product verification system that checks barcodes against a MongoDB database and flags potentially fake items.",
     version="1.0.0"
+)
+origins = [
+    "http://localhost:3000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],            
 )
 
 # MongoDB connection
